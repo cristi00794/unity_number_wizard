78790d7793c5ad92c1f4b0541504c4f350df3c51
Cristache Georgian Cristian <cristiancristache7994@gmail.com>
2018-02-20 17:23:20 +0200

    no message

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string level){
+		Debug.Log ("Level load requested");
+		Application.LoadLevel (level);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit request");
+		Application.Quit ();
+	}
+}
